497a0c8b9c1b0af785ac600290844f865da606ef
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-02-10 23:52:45 +0200

    add redyellow light

--- js/main.js
+++ js/main.js
@@ -17,21 +17,27 @@ const yellow = (colordiv1, colordiv2, colordiv3) => {
     colordiv3.classList.remove('green');
 };
 
+const redyellow = (colordiv1, colordiv2, colordiv3) => {
+    colordiv1.classList.add('red');
+    colordiv2.classList.add('yellow');
+    colordiv3.classList.remove('green');
+};
+
 const green = (colordiv1, colordiv2, colordiv3) => {
     colordiv1.classList.remove('red');
     colordiv2.classList.remove('yellow');
     colordiv3.classList.add('green');
 };
 
-setInterval(() => yellow(red1, yellow1, green1), 5000);
-setInterval(() => red(red1, yellow1, green1), 20000);
-setInterval(() => yellow(red1, yellow1, green1), 30000);
-setInterval(() => green(red1, yellow1, green1), 40000);
-setInterval(() => yellow(red1, yellow1, green1), 45000);
+//setInterval(() => yellow(red1, yellow1, green1), 5000);
+setInterval(() => red(red1, yellow1, green1), 10000);
+setInterval(() => redyellow(red1, yellow1, green1), 20000);
+setInterval(() => green(red1, yellow1, green1), 30000);
+setInterval(() => yellow(red1, yellow1, green1), 40000);
 
 
-setInterval(() => yellow(red2, yellow2, green2), 5000);
-setInterval(() => green(red2, yellow2, green2), 20000);
-setInterval(() => yellow(red2, yellow2, green2), 30000);
-setInterval(() => red(red2, yellow2, green2), 40000);
-setInterval(() => yellow(red2, yellow2, green2), 45000);
+//setInterval(() => yellow(red2, yellow2, green2), 5000);
+setInterval(() => green(red2, yellow2, green2), 10000);
+setInterval(() => yellow(red2, yellow2, green2), 20000);
+setInterval(() => red(red2, yellow2, green2), 30000);
+setInterval(() => redyellow(red2, yellow2, green2), 40000);
