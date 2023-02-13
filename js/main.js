4320495f4f2626cb2dbd01bcf96c5ad4519d7b44
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-02-13 14:33:00 +0200

    add timer

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ const yellow2 = document.querySelector('.light2 > div[name=yellow]');
 const green2 = document.querySelector('.light2 > div[name=green]');
 const count2 = document.querySelector('.light2 > .count');
 
-const red = (colordiv1, colordiv2, colordiv3) => {
+const red = (colordiv1, colordiv2, colordiv3, count) => {
     colordiv1.classList.add('red');
     colordiv2.classList.remove('yellow');
     colordiv3.classList.remove('green');
+    count.classList.remove('greentext');
+    count.classList.add('redtext');
 };
 
 const yellow = (colordiv1, colordiv2, colordiv3) => {
@@ -25,11 +27,13 @@ const redyellow = (colordiv1, colordiv2, colordiv3) => {
     colordiv3.classList.remove('green');
 };
 
-const green = (colordiv1, colordiv2, colordiv3) => {
+const green = (colordiv1, colordiv2, colordiv3, count) => {
     colordiv1.classList.remove('red');
     colordiv2.classList.remove('yellow');
     colordiv3.classList.remove('black');
     colordiv3.classList.add('green');
+    count.classList.remove('redtext');
+    count.classList.add('greentext');
 };
 
 const black = (colordiv3) => {
@@ -41,53 +45,50 @@ const black = (colordiv3) => {
     
 // };
 
-const redt = (count) => {
-    count.classList.remove('greentext');
-    count.classList.add('redtext');
-}
-
-const greent = (count) => {
-    count.classList.remove('redtext');
-    count.classList.add('greentext');
-}
+// const greent = (count) => {
+    
+// }
 
 const delay = 10000;
 let id;
 const delayshag = delay + 1000;
 
-const timer = (count, colort, delayshag) => {
+const timer = (count, delayshag) => {
     delayshag = delayshag - 1000;
     if (delayshag>0) {
-            id = setTimeout(timer, 1000, count, colort, delayshag);
+            id = setTimeout(timer, 1000, count, delayshag);
             count.innerHTML = delayshag/1000;    
     } else {
         count.innerHTML = '--';  
         clearTimeout(id);
+        console.log(delyshag);
+
         return;
     }  
 };
                         
 const render = () => {
-    setTimeout(() => timer(count1, redt(count1), delayshag), delay);
-    setTimeout(() => red(red1, yellow1, green1), delay);       
+    
+    setTimeout(() => red(red1, yellow1, green1, count1), delay);       
+    setTimeout(() => timer(count1, delayshag), delay);
     setTimeout(() => redyellow(red1, yellow1, green1), delay+10000);
-    setTimeout(() => timer(count1, greent(count1), delayshag), delay+20000);
-    setTimeout(() => green(red1, yellow1, green1), delay+20000);
+    setTimeout(() => green(red1, yellow1, green1, count1), delay+20000);
+    setTimeout(() => timer(count1, delayshag), delay+20000);
     setTimeout(() => black(green1), delay+26000);
     setTimeout(() => green(red1, yellow1, green1), delay+27000);
     setTimeout(() => black(green1), delay+28000);
     setTimeout(() => green(red1, yellow1, green1), delay+29000);
     setTimeout(() => yellow(red1, yellow1, green1), delay+30000);
 
-    setTimeout(() => timer(count2, greent(count2), delayshag), delay);
-    setTimeout(() => green(red2, yellow2, green2), delay);
+    setTimeout(() => green(red2, yellow2, green2, count2), delay);
+    setTimeout(() => timer(count2, delayshag), delay);
     setTimeout(() => black(green2), delay+6000);
-    setTimeout(() => green(red2, yellow2, green2), delay+7000);
+    setTimeout(() => green(red2, yellow2, green2, count2), delay+7000);
     setTimeout(() => black(green2), delay+8000);
-    setTimeout(() => green(red2, yellow2, green2), delay+9000);
+    setTimeout(() => green(red2, yellow2, green2, count2), delay+9000);
     setTimeout(() => yellow(red2, yellow2, green2), delay+10000);
-    setTimeout(() => timer(count2, redt(count2), delayshag), delay+20000);
-    setTimeout(() => red(red2, yellow2, green2), delay+20000);
+    setTimeout(() => red(red2, yellow2, green2, count2), delay+20000);
+    setTimeout(() => timer(count2, delayshag), delay+20000);
     setTimeout(() => redyellow(red2, yellow2, green2), delay+30000);
 };
 
@@ -95,7 +96,7 @@ render();
 
 setInterval( () => {
     render();
-}, 40000);
+}, delay+30000);
 
 
 
